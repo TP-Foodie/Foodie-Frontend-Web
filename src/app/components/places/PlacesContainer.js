5d7674a06afcc444253fdbad6b6100226056e902
Facundo Gerstner <fgerstner@devartis.com>
2019-09-01 17:23:16 -0300

    add initial fetch

--- src/app/components/places/PlacesContainer.js
+++ src/app/components/places/PlacesContainer.js
@@ -1,6 +1,12 @@
 import React from "react";
+import httpResources from "../../http/httpResources";
 
 export class PlacesContainer extends React.Component {
+
+    componentDidMount = () => {
+        httpResources.places();
+    };
+
     render() {
         return <div className={"container"}/>
     }
--- src/test/places/PlacesContainer.test.js
+++ src/test/places/PlacesContainer.test.js
@@ -1,10 +1,28 @@
 import React from "react";
-import {shallow} from "enzyme";
+import {mount, shallow} from "enzyme";
 import {PlacesContainer} from "../../app/components/places/PlacesContainer";
+import httpResources from "../../app/http/httpResources";
+
+jest.mock("../../app/http/httpResources", () => {
+    return {
+        places: jest.fn()
+    }
+});
 
 describe("places container component", () => {
     it ("should render without crashing", () => {
         const component = shallow(<PlacesContainer/>);
         expect(component.find('.container').exists()).toBe(true);
+    });
+
+    describe("init cycle", () => {
+
+        beforeEach(() => {
+            mount(<PlacesContainer/>)
+        });
+
+        it ("should fetch places", () => {
+            expect(httpResources.places).toHaveBeenCalled();
+        });
     });
 });
