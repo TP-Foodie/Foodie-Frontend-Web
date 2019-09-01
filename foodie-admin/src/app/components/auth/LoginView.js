90d36fc1e9ac5d1760c3d467fc92cf4b28566d46
Facundo Gerstner <fgerstner@devartis.com>
2019-09-01 14:24:44 -0300

    adjust styling

--- foodie-admin/src/app/components/auth/LoginView.js
+++ foodie-admin/src/app/components/auth/LoginView.js
@@ -1,14 +1,24 @@
 import React from "react";
-import { Button } from '@material-ui/core';
+import {Button, Grid} from '@material-ui/core';
+
+const LOGIN_TEXT = "LOGIN";
 
 export class LoginView extends React.Component {
     render() {
         return (
-            <div className={"container"}>
-                <Button className={'login_btn'} href={''}>
-
-                </Button>
-            </div>
+            <Grid
+                container
+                direction="column"
+                alignItems="center"
+                justify="center"
+                style={{ minHeight: '100vh' }}
+            >
+                <Grid item>
+                    <Button variant="contained" color="primary" className={'login_btn'} href={''}>
+                        {LOGIN_TEXT}
+                    </Button>
+                </Grid>
+            </Grid>
         );
     }
 }
