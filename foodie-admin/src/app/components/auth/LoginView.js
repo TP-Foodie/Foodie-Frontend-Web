68717382495f62746c356e3cc08dd35e6badb259
Facundo Gerstner <fgerstner@devartis.com>
2019-09-01 12:49:35 -0300

    setup material ui and add login button

--- foodie-admin/src/app/components/auth/LoginView.js
+++ foodie-admin/src/app/components/auth/LoginView.js
@@ -1,7 +1,14 @@
 import React from "react";
+import { Button } from '@material-ui/core';
 
 export class LoginView extends React.Component {
     render() {
-        return <div/>;
+        return (
+            <div className={"container"}>
+                <Button className={'login_btn'} href={''}>
+
+                </Button>
+            </div>
+        );
     }
 }
--- foodie-admin/src/test/auth/LoginView.test.js
+++ foodie-admin/src/test/auth/LoginView.test.js
@@ -5,6 +5,18 @@ import {LoginView} from "../../app/components/auth/LoginView";
 describe("login view component", () => {
     it ("should render without crashing", () => {
         const component = shallow(<LoginView/>);
-        expect(component).toMatchSnapshot();
+        expect(component.find('.container').exists()).toBe(true);
+    });
+
+    describe("elements", () => {
+        let component;
+
+        beforeEach(() => {
+            component = shallow(<LoginView/>);
+        });
+
+        it ("should render login button", () => {
+            expect(component.find('.login_btn').exists()).toBe(true);
+        });
     });
 });
