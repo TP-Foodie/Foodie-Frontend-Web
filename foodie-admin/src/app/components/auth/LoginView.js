48687f59bcef458794090757cbec0fdc5384cf36
Facundo Gerstner <fgerstner@devartis.com>
2019-09-01 15:04:05 -0300

    wire up screens

--- foodie-admin/src/app/components/auth/LoginView.js
+++ foodie-admin/src/app/components/auth/LoginView.js
@@ -1,5 +1,6 @@
 import React from "react";
 import {Button, Grid} from '@material-ui/core';
+import {WELCOME} from "../../navigation/routes";
 
 const LOGIN_TEXT = "LOGIN";
 
@@ -14,7 +15,7 @@ export class LoginView extends React.Component {
                 style={{ minHeight: '100vh' }}
             >
                 <Grid item>
-                    <Button variant="contained" color="primary" className={'login_btn'} href={''}>
+                    <Button variant="contained" color="primary" className={'login_btn'} href={WELCOME}>
                         {LOGIN_TEXT}
                     </Button>
                 </Grid>
