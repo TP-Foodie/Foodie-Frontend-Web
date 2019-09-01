2f87bdce87d45a15723a30b94c6cdc659220b621
Facundo Gerstner <fgerstner@devartis.com>
2019-09-01 18:33:32 -0300

    render places' names

--- src/app/components/places/PlacesView.js
+++ src/app/components/places/PlacesView.js
@@ -1,7 +1,29 @@
 import React from "react";
+import Grid from "@material-ui/core/Grid";
+import List from "@material-ui/core/List";
+import {ListItemText} from "@material-ui/core";
+import PropTypes from "prop-types";
 
 export class PlacesView extends React.Component {
+    static propTypes = {
+        places: PropTypes.array.isRequired
+    };
+
     render() {
-        return <div className={"container"}/>
+        const {places} = this.props;
+
+        return(
+            <Grid container className={"container"}>
+                <Grid item xs={places.length}>
+                    <List className={"places_list"}>
+                        {places.map(place =>
+                            <ListItemText key={place.id} className={place.name}>
+                                {place.name}
+                            </ListItemText>
+                        )}
+                    </List>
+                </Grid>
+            </Grid>
+        );
     }
 }
--- src/test/places/PlacesView.test.js
+++ src/test/places/PlacesView.test.js
@@ -1,10 +1,41 @@
 import {shallow} from "enzyme/build";
 import React from "react";
 import {PlacesView} from "../../app/components/places/PlacesView";
+import {aPlace} from "../support/factory";
 
 describe("places view component", () => {
+    let props;
+    let render;
+
+    beforeEach(() => {
+        props = {
+            places: [aPlace()]
+        };
+
+        render = props => shallow(<PlacesView {...props}/>);
+    });
+
     it("should render without crashing", () => {
-        const component = shallow(<PlacesView/>);
+        const component = render(props);
         expect(component.find('.container').exists()).toBe(true);
+    });
+
+    describe("elements", () => {
+        let component;
+
+        beforeEach(() => {
+            component = render(props);
+        });
+
+        it ("should render a list", () => {
+            expect(component.find('.places_list').exists()).toBeTruthy();
+        });
+
+        it ("should render each place name", () => {
+            // eslint-disable-next-line no-unused-vars
+            for (const place of props.places) {
+                expect(component.find(`.${place.name}`).exists()).toBeTruthy();
+            }
+        });
     });
 });
