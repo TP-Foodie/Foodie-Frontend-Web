0c6917e5c9e6396fce76817ebd207d931ab7a4ef
Facundo Gerstner <fgerstner@devartis.com>
2019-09-01 14:37:49 -0300

    setup routing

--- foodie-admin/src/index.js
+++ foodie-admin/src/index.js
@@ -3,6 +3,18 @@ import ReactDOM from 'react-dom';
 import './app/styles/index.css';
 import App from './app/components/App';
 import * as serviceWorker from './serviceWorker';
+import { Route, Link, BrowserRouter as Router } from 'react-router-dom'
+import {LoginView} from "./app/components/auth/LoginView";
+import {LOGIN} from "./app/navigation/routes";
+
+const routing = (
+    <Router>
+        <div>
+            <Route exact path="/" component={App} />
+            <Route path={LOGIN} component={LoginView} />
+        </div>
+    </Router>
+);
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
