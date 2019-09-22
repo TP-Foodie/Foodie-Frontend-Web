b44b250eeba6604236ae44ec50ef29f1c077b1b2
Facundo Gerstner <fgerstner@devartis.com>
2019-09-22 17:12:59 -0300

    complete general layout

--- src/app/components/welcome/GeneralLayout.js
+++ src/app/components/welcome/GeneralLayout.js
@@ -1,32 +1,109 @@
-import React from "react";
-import AppBar from "@material-ui/core/AppBar";
-import Toolbar from "@material-ui/core/Toolbar";
-import IconButton from "@material-ui/core/IconButton";
-import Typography from "@material-ui/core/Typography";
-import {Menu} from "@material-ui/icons";
+import React from 'react';
+import clsx from 'clsx';
+import { makeStyles, useTheme } from '@material-ui/core/styles';
+import Drawer from '@material-ui/core/Drawer';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import List from '@material-ui/core/List';
+import Typography from '@material-ui/core/Typography';
+import Divider from '@material-ui/core/Divider';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
+import ChevronRightIcon from '@material-ui/icons/ChevronRight';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
 import {styles} from "../../styles/common";
+import {Map, Person} from "@material-ui/icons";
+import ListItemIcon from "@material-ui/core/ListItemIcon";
 import Avatar from "@material-ui/core/Avatar";
+import PropTypes from "prop-types";
 
+const useStyles = makeStyles(styles.generalLayoutStyles);
 const TITLE = "Plataforma de administración Foodie";
+const MODULES = {
+    'Usuarios': <Person/>,
+    'Lugares': <Map/>
+};
 
-export class GeneralLayout extends React.Component {
-    render() {
-        return (
-            <div>
-                <AppBar position="static" style={styles.main_gradient}>
-                    <Toolbar>
-                        <IconButton edge="start" color="inherit" aria-label="menu">
-                            <Menu/>
-                        </IconButton>
-                        <Typography variant="h6" style={styles.flex_grow}>
-                            {TITLE}
-                        </Typography>
-                        <IconButton edge={"end"}>
-                            <Avatar>A</Avatar>
-                        </IconButton>
-                    </Toolbar>
-                </AppBar>
-            </div>
-        );
+export const GeneralLayout = props => {
+    const classes = useStyles();
+    const theme = useTheme();
+    const [open, setOpen] = React.useState(false);
+
+    function handleDrawerOpen() {
+        setOpen(true);
     }
-}
+
+    function handleDrawerClose() {
+        setOpen(false);
+    }
+
+    return (
+        <div className={classes.root}>
+            <CssBaseline />
+            <AppBar
+                position="fixed"
+                className={clsx(classes.appBar, {
+                    [classes.appBarShift]: open,
+                })}
+                style={styles.main_gradient}
+            >
+                <Toolbar>
+                    <IconButton
+                        color="inherit"
+                        aria-label="open drawer"
+                        onClick={handleDrawerOpen}
+                        edge="start"
+                        className={clsx(classes.menuButton, open && classes.hide)}
+                    >
+                        <MenuIcon />
+                    </IconButton>
+                    <Typography variant="h6" style={styles.flex_grow}>
+                        {TITLE}
+                    </Typography>
+                    <IconButton edge={"end"}>
+                        <Avatar>A</Avatar>
+                    </IconButton>
+                </Toolbar>
+            </AppBar>
+            <Drawer
+                className={classes.drawer}
+                variant="persistent"
+                anchor="left"
+                open={open}
+                classes={{
+                    paper: classes.drawerPaper,
+                }}
+            >
+                <div className={classes.drawerHeader}>
+                    <IconButton onClick={handleDrawerClose}>
+                        {theme.direction === 'ltr' ? <ChevronLeftIcon /> : <ChevronRightIcon />}
+                    </IconButton>
+                </div>
+                <Divider />
+                <List>
+                    {Object.keys(MODULES).map(moduleName => (
+                        <ListItem button key={moduleName}>
+                            <ListItemIcon>{MODULES[moduleName]}</ListItemIcon>
+                            <ListItemText primary={moduleName} />
+                        </ListItem>
+                    ))}
+                </List>
+            </Drawer>
+            <main
+                className={clsx(classes.content, {
+                    [classes.contentShift]: open,
+                })}
+            >
+                <div className={classes.drawerHeader} />
+                {props.children}
+            </main>
+        </div>
+    );
+};
+
+GeneralLayout.propTypes = {
+    children: PropTypes.oneOf([PropTypes.object, PropTypes.array])
+};
--- src/app/styles/common.js
+++ src/app/styles/common.js
@@ -1,3 +1,5 @@
+const DRAWER_WIDTH = 240
+
 export const styles = {
     pd_full: {
         padding: 20
@@ -44,5 +46,62 @@ export const styles = {
 
     heading: {
         color: "#FFFFFF"
-    }
+    },
+    
+    generalLayoutStyles: theme => ({
+        root: {
+            display: 'flex',
+        },
+        appBar: {
+            transition: theme.transitions.create(['margin', 'width'], {
+                easing: theme.transitions.easing.sharp,
+                duration: theme.transitions.duration.leavingScreen,
+            }),
+        },
+        appBarShift: {
+            width: `calc(100% - ${240}px)`,
+            marginLeft: DRAWER_WIDTH,
+            transition: theme.transitions.create(['margin', 'width'], {
+                easing: theme.transitions.easing.easeOut,
+                duration: theme.transitions.duration.enteringScreen,
+            }),
+        },
+        menuButton: {
+            marginRight: theme.spacing(2),
+        },
+        hide: {
+            display: 'none',
+        },
+        drawer: {
+            width: DRAWER_WIDTH,
+            flexShrink: 0,
+        },
+        drawerPaper: {
+            width: DRAWER_WIDTH,
+        },
+        drawerHeader: {
+            display: 'flex',
+            alignItems: 'center',
+            padding: theme.spacing(0, 1),
+            ...theme.mixins.toolbar,
+            justifyContent: 'flex-end',
+        },
+        content: {
+            flexGrow: 1,
+            padding: theme.spacing(3),
+            transition: theme.transitions.create('margin', {
+                easing: theme.transitions.easing.sharp,
+                duration: theme.transitions.duration.leavingScreen,
+            }),
+            marginLeft: -DRAWER_WIDTH,
+        },
+        contentShift: {
+            transition: theme.transitions.create('margin', {
+                easing: theme.transitions.easing.easeOut,
+                duration: theme.transitions.duration.enteringScreen,
+            }),
+            marginLeft: 0,
+        },
+    })
 };
+
