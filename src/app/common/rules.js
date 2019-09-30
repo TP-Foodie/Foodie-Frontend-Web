08672daa1a08b3f99f4b64cb0379821d3b40dd33
Facundo Gerstner <fgerstner@devartis.com>
2019-09-30 15:49:52 -0300

    add rules

--- src/app/common/rules.js
+++ src/app/common/rules.js
@@ -1,3 +1,12 @@
 export const LOGIN_RULES = {
-
+    email: {
+        email: {
+            message: "^Por favor ingrese un email valido"
+        }
+    },
+    password: {
+        presence: {
+            message: "^Por favor ingrese una contraseña valida"
+        }
+    }
 };
