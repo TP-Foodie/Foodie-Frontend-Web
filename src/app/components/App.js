63a71df1f5bd795dd1db76358cbe49b4f243a154
Azowyl <facugerstner_29@hotmail.com>
2019-10-04 16:28:19 -0300

    setup redux

--- src/app/components/App.js
+++ src/app/components/App.js
@@ -1,10 +1,32 @@
 import React from 'react';
-import '../styles/App.css';
-import {LoginView} from "./auth/LoginView";
+import { Route, Switch, BrowserRouter as Router } from 'react-router-dom'
+import {LoginContainer} from "./auth/LoginContainer";
+import {AuthLoadingContainer} from "./auth/AuthLoadingContainer";
+import {LOGIN, PLACES, WELCOME, USERS} from "../navigation/routes";
+import {WelcomeView} from "./welcome/WelcomeView";
+import {PlacesContainer} from "./places/PlacesContainer";
+import {UsersContainer} from "./users/UsersContainer";
+import {UserDetailContainer} from "./users/UserDetailContainer";
+import PrivateRoute from "./utils/PrivateRoute";
+import 'typeface-roboto';
+import { GeneralLayout } from './utils/GeneralLayout';
 
 function App() {
   return (
-      <LoginView/>
+    <div>
+        <Router>
+            <Switch>
+                <GeneralLayout>
+                    <Route exact path="/" component={AuthLoadingContainer} />
+                    <Route path={LOGIN} component={LoginContainer} />
+                    <PrivateRoute path={WELCOME} component={WelcomeView} />
+                    <PrivateRoute path={PLACES} component={PlacesContainer} />
+                    <PrivateRoute path={USERS} component={UsersContainer} exact/>
+                    <PrivateRoute path={`${USERS}/:userId`} component={UserDetailContainer} />
+                </GeneralLayout>
+            </Switch>
+        </Router>
+    </div>
   );
 }
 
