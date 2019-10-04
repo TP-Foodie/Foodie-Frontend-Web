38d7440b02803c35a82024882b3dae8b8b9ce931
Azowyl <facugerstner_29@hotmail.com>
2019-10-04 16:53:43 -0300

    add rules fetching and setting logic

--- src/app/components/rules/RulesListContainer.js
+++ src/app/components/rules/RulesListContainer.js
@@ -1,10 +1,36 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import { RulesListView } from "./RulesListView";
+import {setLoading} from "../../redux/reducers/loading";
+import {connect} from "react-redux";
+import {httpResources} from "../../http/httpResources";
+import {handleError} from "../../handlers/handleError";
+import PropTypes from "prop-types";
 
 const RulesListContainer = props => {
+    const [rules, setRules] = useState([]);
+
+    useEffect(async () => {
+        try {
+            props.setLoading(true);
+            const {data} = await httpResources.rules();
+            setRules(data);
+            props.setLoading(false);
+        } catch (error) {
+            handleError(error);
+        }
+    })
+
     return (
-        <RulesListView/>
+        <RulesListView rules={rules}/>
     );
 }
 
-export default RulesListContainer
+RulesListContainer.propTypes = {
+    setLoading: PropTypes.func.isRequired,
+}
+
+const mapDispatchToProps = {
+    setLoading
+};
+
+export default connect(undefined, mapDispatchToProps)(RulesListContainer);
