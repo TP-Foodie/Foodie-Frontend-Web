f5fb36f9654b21277d3ee0638455244b90f374be
azowyl <facugerstner_29@hotmail.com>
2019-10-27 17:29:47 -0300

    eslint fix

--- src/app/components/rules/RuleConditionForm.js
+++ src/app/components/rules/RuleConditionForm.js
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React from "react";
 import Grid from "@material-ui/core/Grid";
 import { TextField, IconButton, Button } from "@material-ui/core";
 import MenuItem from '@material-ui/core/MenuItem';
