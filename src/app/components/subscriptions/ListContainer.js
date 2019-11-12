782cc89a90c5da3f722a01a3d73401ebbadd7f56
Facu G <fgerstner@devartis.com>
2019-11-12 15:05:33 -0300

    add container logic

--- src/app/components/subscriptions/ListContainer.js
+++ src/app/components/subscriptions/ListContainer.js
@@ -1,8 +1,40 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {SubscriptionListView} from './ListView';
+import {setLoading} from '../../redux/reducers/loading';
+import {connect} from 'react-redux';
+import {handleError} from '../../redux/reducers/handlers';
+import httpResources from '../../http/httpResources';
+import PropTypes from 'prop-types';
 
 export const SubscriptionListContainer = props => {
-    return <SubscriptionListView/>;
+    const [users, setUsers] = useState([]);
+    const {setLoading, handleError} = props;
+
+    useEffect(() => {
+        async function fetch() {
+            setLoading(true);
+            try {
+                const {data} = await httpResources.users();
+                setUsers(data.users);
+            } catch (error) {
+                handleError(error);
+            }
+            setLoading(false);
+        }
+        fetch();
+    }, [setLoading, handleError])
+
+    return <SubscriptionListView users={users}/>;
 }
 
-export default SubscriptionListContainer;
+SubscriptionListContainer.propTypes = {
+    setLoading: PropTypes.func.isRequired,
+    handleError: PropTypes.func.isRequired,
+}
+
+const mapDispatchToProps = {
+    setLoading,
+    handleError
+}
+
+export default connect(undefined, mapDispatchToProps)(SubscriptionListContainer);
