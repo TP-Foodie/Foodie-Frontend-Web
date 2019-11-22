2b63b12ba3b636dfd4e04449a5c77ad402f780ca
Facu G <fgerstner@devartis.com>
2019-11-22 13:59:22 -0300

    add labels

--- src/app/components/statistics/AdminGraph.js
+++ src/app/components/statistics/AdminGraph.js
@@ -1,13 +1,13 @@
 import React from 'react';
-import { AreaChart, Area, CartesianGrid, XAxis, YAxis, Tooltip, Legend } from 'recharts';
+import { AreaChart, Area, CartesianGrid, XAxis, YAxis, Tooltip, Legend, Label } from 'recharts';
 import PropTypes from 'prop-types';
 
 export const AdminGraph = props => {
     return (
         <AreaChart width={400} height={400} data={props.data}>
             <CartesianGrid stroke="#ccc" strokeDasharray="5 5"/>
-            <XAxis dataKey="x" />
-            <YAxis />
+            <XAxis dataKey="x"><Label value="Fecha" position="bottom"/></XAxis>
+            <YAxis><Label value="Cantidad" position="left" /></YAxis>
             <Area type="monotone" dataKey="y" stroke="#8884d8" fill="#8884d8" />
             <Tooltip />
             <Legend content={<h3>{props.title}</h3>}/>
