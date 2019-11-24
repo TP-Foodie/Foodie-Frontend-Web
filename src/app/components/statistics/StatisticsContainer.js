390ec16d5e3c318dea7f6ae1b36f10bbfd2294f5
azowyl <facugerstner_29@hotmail.com>
2019-11-24 15:00:14 -0300

    hooks fix

--- src/app/components/statistics/StatisticsContainer.js
+++ src/app/components/statistics/StatisticsContainer.js
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from 'react';
+import React, {useState, useEffect, useCallback} from 'react';
 import {setLoading} from '../../redux/reducers/loading';
 import {handleError} from '../../redux/reducers/handlers';
 import { AdminGraph } from './AdminGraph';
@@ -10,22 +10,22 @@ import {Parser} from '../../common/parser';
 export const StatisticsContainer = props => {
     const [data, setData] = useState([]);
     const [selectedDate, setSelectedDate] = useState(new Date())
-    const {setLoading, handleError} = props;
+    const {setLoading, handleError, fetchData} = props;
 
-    const fetchData = async (date = new Date()) => {        
+    const memoizedFetchData = useCallback(async (date = new Date()) => {        
         try {
             setLoading(true);
-            const {data} = await props.fetchData(date);
+            const {data} = await fetchData(date);
             setData(Parser.parseDateForChart(data));
         } catch (error) {
             handleError(error);
         }
         setLoading(false);
-    }
+    }, [fetchData, handleError, setLoading])
 
     useEffect(() => {
-        fetchData()
-    }, [fetchData]);
+        memoizedFetchData()
+    }, [memoizedFetchData]);
 
     const onChangeDate = date => {
         setSelectedDate(date);
